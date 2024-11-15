60dcf11fe8d677f4f19be4acef8aedc6a7633a89
Dinhero21 <dinhero21@dinhero21.dev>
2024-11-15 06:02:06 -0300

    better reflect
    this doesn't get fucked anymore

--- src/util/reflect.ts
+++ src/util/reflect.ts
@@ -1,8 +1,18 @@
 export function override<T, K extends keyof T>(
   target: T,
   key: K,
-  callback: (original: T[K]) => T[K],
+  callback: (
+    original: T[K],
+  ) => /* this could have been simply T[K] but then it wouldn't scope functions correctly */
+  T[K] extends (...args: infer TArgs) => infer TReturnType
+    ? (this: T, ...args: TArgs) => TReturnType
+    : T[K],
 ): void {
-  const original = target[key];
-  target[key] = callback(original);
+  let original = target[key];
+
+  if (typeof original === 'function') {
+    original = original.bind(target);
+  }
+
+  target[key] = callback(original) as T[K];
 }
