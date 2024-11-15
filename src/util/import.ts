5a94a95a7bcef1551857783c37e49a87e157606b
Dinhero21 <dinhero21@dinhero21.dev>
2024-11-15 06:01:41 -0300

    ignore non-js files on import directory

--- src/util/import.ts
+++ src/util/import.ts
@@ -5,6 +5,8 @@ export async function importDirectory(path: string): Promise<void> {
   for (const entry of await readdir(path, { withFileTypes: true })) {
     if (!entry.isFile()) continue;
 
+    if (!entry.name.endsWith('.js')) continue;
+
     await import(resolve(path, entry.name));
   }
 }
