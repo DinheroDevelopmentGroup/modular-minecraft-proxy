1cd9d5b3ef4a86addcb19754c9380e5bad3f9b88
Dinhero21 <dinhero21@dinhero21.dev>
2024-11-15 06:01:29 -0300

    better copy script

--- script/copy.sh
+++ script/copy.sh
@@ -1,11 +1,24 @@
 #!/usr/bin/env bash
 
-# hopefully excluding all ts files
-# instead of including all node_modules
-# doesn't come to bite me in the ass
-# later
-rsync \
-  --verbose \
-  -a src/ dist \
-  --exclude "*.ts" \
-  $@
+# add symlinks
+find src -type d -name "node_modules" | while read -r dir_src; do
+  dir=${dir_src#src/}
+  dir_dist="dist/${dir}"
+
+  mkdir -p "$(dirname "$dir_dist")"
+
+  if [[ ! -L "$dir_dist" ]]; then
+    rm -rf "$dir_dist"
+    ln -sTrf "$dir_src" "$dir_dist"
+  fi
+done
+
+# remove symlinks
+find dist -type l -name "node_modules" | while read -r link_dist; do
+  link=${link_dist#dist/}
+  link_src="src/${link}"
+
+  if [[ ! -d "$link_src" ]]; then
+    rm "$link_dist"
+  fi
+done
